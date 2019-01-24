05840ace4b8fd4d72f6ec326de56ae3bdcc5b3ee
Jorge Fragoza <jorge.fragoza@Fragoza-MacBook-Pro.local>
2019-01-24 00:13:57 -0700

    usuario correcto jorge.fragoza

--- login.js
+++ login.js
@@ -3,7 +3,7 @@ const events = require('./login-events');
 class Login {
     constructor() {
         this.login = (user)  => {
-            if (user === 'jorge' ) {
+            if (user === 'jorge.fragoza' ) {
                 EventHandler.publish(new events.LoginSuccess(user));
             } else {
                 EventHandler.publish(new events.LoginError());
